74a945141313f1bb463ab6d44d122d3e00e44ea9
brandonejhall <brandonejhall7@outlook.com>
2021-11-05 01:09:29 -0500

    Ex1-Ex3

---
+++ world.js
@@ -0,0 +1,32 @@
+"use strict";
+window.onload = function(){
+    
+    var btn = document.getElementById('lookup');
+    var search = document.getElementById('country');
+    var url = 'http://localhost/info2180-lab5/world.php?country=';
+    var result = document.getElementById('result');
+
+    
+    btn.addEventListener('click',function(event){
+        event.preventDefault();
+        let query_element =  search.value;
+        let request = new URL(url+query_element);
+        fetch (request)
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                } 
+                else{
+                    return Promise.reject('something went wrong')
+                }
+        })
+            .then (function(data){
+            result.innerHTML = data;
+        })
+            .catch (error => console.log('There was an error: ' + error))
+    
+})
+
+
+    }
+
--- world.php
+++ world.php
@@ -1,13 +1,19 @@
+
+<?php if ($_SERVER['REQUEST_METHOD']=== 'GET'):?>
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
+$country = filter_input(INPUT_GET, 'country',FILTER_SANITIZE_STRING,
+FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 
 ?>
 <ul>
@@ -15,3 +21,4 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
 <?php endforeach; ?>
 </ul>
+<?php endif ?>
