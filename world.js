85905db35c658c0f6c2e6fd5f2fc87768f8bd265
brandonejhall <brandonejhall7@outlook.com>
2021-11-07 03:44:41 -0500

    Ex5

--- world.js
+++ world.js
@@ -2,6 +2,7 @@
 window.onload = function(){
     
     var btn = document.getElementById('lookup');
+    var city = document.getElementById('cities');
     var search = document.getElementById('country');
     var url = 'http://localhost/info2180-lab5/world.php?country=';
     var result = document.getElementById('result');
@@ -10,7 +11,7 @@ window.onload = function(){
     btn.addEventListener('click',function(event){
         event.preventDefault();
         let query_element =  search.value;
-        let request = new URL(url+query_element);
+        let request = new URL(url+query_element+'&context=') ;
         fetch (request)
             .then(response => {
                 if (response.ok){
@@ -26,6 +27,25 @@ window.onload = function(){
             .catch (error => console.log('There was an error: ' + error))
     
 })
+
+    city.addEventListener('click',function(event){
+        event.preventDefault();
+        let query_element = search.value;
+        let request = new URL(url + query_element + '&context=cities');
+        fetch (request)
+            .then(response => {
+                if (response.ok){
+                    return response.text()
+                } 
+                else{
+                    return Promise.reject('something went wrong')
+                }
+        })
+            .then (function(data){
+            result.innerHTML = data;
+        })
+            .catch (error => console.log('There was an error: ' + error))
+    })
 
 
     }
--- world.php
+++ world.php
@@ -9,15 +9,24 @@ $dbname = 'world';
 $country = filter_input(INPUT_GET, 'country',FILTER_SANITIZE_STRING,
 FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
 
+$city = filter_input(INPUT_GET, 'context',FILTER_SANITIZE_STRING,
+FILTER_FLAG_STRIP_LOW | FILTER_FLAG_STRIP_HIGH);
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$all_cities = $conn->query("SELECT cities.name, cities.district, cities.population 
+FROM cities 
+JOIN countries 
+ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
 
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$city_results = $all_cities->fetchAll(PDO::FETCH_ASSOC)
 
 ?>
 
 
+<?php if ($city == ''):?>
 
 <table>
 <tr>
@@ -25,6 +34,7 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <th>Continent</th>
   <th>Independence</th>
   <th>Head of State</th>
+</tr>
 
   <?php foreach ($results as $row): ?>
   <tr>
@@ -36,4 +46,20 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   <?php endforeach; ?>
 
 </table>
+<?php elseif($city == 'cities'): ?>
+
+<table>
+  <tr>
+    <th>Name</th>
+    <th>District</th> 
+    <th>Population</th> 
+  </tr>
+  <?php foreach ($city_results as $row): ?>
+  <tr>
+    <td><?= $row['name'] ?></td>
+    <td><?= $row['district'] ?></td>
+    <td><?= $row['population'] ?></td>
+  </tr>
+  <?php endforeach; ?>
+<?php endif ?>
 <?php endif ?>
